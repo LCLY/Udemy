b6009de9d90dcae99701460e29588cdef8b4098b
lchoo <lchoo1996@gmail.com>
2019-11-06 01:06:32 +0800

    refactor: grouping

--- Complete JavaScript Course/Tutorial 1/script.js
+++ Complete JavaScript Course/Tutorial 1/script.js
@@ -12,11 +12,11 @@ job = "Yes";
 
 //var 3 = 3; //error because you can only declare a var with _ $ or letters
 
-// variable mutation and type coercion
+/* ========== variable mutation and type coercion ========== */
 var firstName = "john";
 var age = 28;
 
-// Type coercion
+/* ========== Type coercion =========== */
 // console.log(firstName + " " + age); // this will output a string of john 28
 
 var job, isMarried;
@@ -24,7 +24,7 @@ job = "teacher";
 isMarried = false;
 // console.log(firstName + age + job + isMarried);
 
-// variable mutation
+/* ========== Variable mutation =========== */
 age = "twenty eight";
 job = "driver";
 
@@ -33,7 +33,7 @@ job = "driver";
 // var lastName = prompt("What is the last name?");
 // console.log(firstname + " " + lastName);
 
-// basic operators
+/* ========== Math operators =========== */
 var year, yearJohn, yearMark;
 now = 2018;
 ageJohn = 28;
@@ -41,14 +41,31 @@ ageMark = 33;
 
 yearJohn = now - ageJohn;
 yearMark = now - ageMark;
-console.log(yearJohn);
+// console.log(yearJohn);
 
-console.log(now + 2);
-console.log(now * 2);
-console.log(now / 10);
+// console.log(now + 2);
+// console.log(now * 2);
+// console.log(now / 10);
 
-// logical operators
+/* ========== logical operators =========== */
 var johnOlder = ageJohn > ageMark;
-console.log(johnOlder);
-console.log(typeof johnOlder);
-console.log(typeof x);
+// console.log(johnOlder);
+
+/* ========== type operators =========== */
+// console.log(typeof johnOlder);
+// console.log(typeof x);
+
+/* ========== Operator precedence (which operator executed first) =========== */
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+/* ========== multiple operators =========== */
+var isFullAge = now - yearJohn >= fullAge;
+// console.log(isFullAge);
+
+/* ========== Grouping (basically parenthesis) =========== */
+var ageJohn = now - yearJohn;
+var agemark = 35;
+var average = (ageJohn + agemark) / 2;
+// console.log(average);
