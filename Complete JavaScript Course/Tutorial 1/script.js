3dbf34153ba0bc72fc94e6fc6b24f61513d102df
lchoo <lchoo1996@gmail.com>
2019-11-05 22:41:06 +0800

    refactor: learn type coercion

--- Complete JavaScript Course/Tutorial 1/script.js
+++ Complete JavaScript Course/Tutorial 1/script.js
@@ -7,7 +7,19 @@ console.log(assignSomething);
 var job;
 console.log(job); //this will be undefined
 
-job = 'Yes';
-console.log(job) // now it will be fined
+job = "Yes";
+console.log(job); // now it will be fined
 
-var 3 = 3; //error because you can only declare a var with _ $ or letters
+//var 3 = 3; //error because you can only declare a var with _ $ or letters
+
+// variable mutation and type coercion
+var firstName = "john";
+var age = 28;
+
+// Type coercion
+console.log(firstName + " " + age); // this will output a string of john 28
+
+var job, isMarried;
+job = "teacher";
+isMarried = false;
+console.log(firstName + age + job + isMarried);
