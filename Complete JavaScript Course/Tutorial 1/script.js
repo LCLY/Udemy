df65c5bf2d14736482b3d28671f19dc88deb93ba
lchoo <lchoo1996@gmail.com>
2019-11-05 22:32:56 +0800

    refactor: variables rules

--- Complete JavaScript Course/Tutorial 1/script.js
+++ Complete JavaScript Course/Tutorial 1/script.js
@@ -3,3 +3,11 @@ console.log(firstname);
 
 var assignSomething = "yes";
 console.log(assignSomething);
+
+var job;
+console.log(job); //this will be undefined
+
+job = 'Yes';
+console.log(job) // now it will be fined
+
+var 3 = 3; //error because you can only declare a var with _ $ or letters
