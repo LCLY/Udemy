ac16206d9dfed771be6bed0505eb4ffc9d58d674
LCLY <lchoo9294@gmail.com>
2019-11-07 17:40:54 +0800

    testing commit

--- Complete JavaScript Course/JS Game/challenges.js
+++ Complete JavaScript Course/JS Game/challenges.js
@@ -6,6 +6,9 @@ Change the game to follow these rules:
 2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
 3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
 */
+
+// testing
+
 var scores, roundScore, activePlayer, gamePlaying, lastDice;
 init();
 // console.log(dice);
