4c0936b0380694093b4d9b96745676d56b7dc528
LCLY <lchoo9294@gmail.com>
2019-11-08 02:59:02 +0800

    challenge: making use of prototype

--- Complete JavaScript Course/Objects and functions/challenge.js
+++ Complete JavaScript Course/Objects and functions/challenge.js
@@ -46,6 +46,47 @@
 
 /* ========== solution ========== */
 // include the whole block of code into the II
+// (function() {
+//     function Question(question, answers, correct) {
+//         this.question = question;
+//         this.answers = answers;
+//         this.correct = correct;
+//     }
+
+//     Question.prototype.displayQuestion = function() {
+//         console.log(this.question); //this will point to respective q
+//         for (var i = 0; i < this.answers.length; i++) {
+//             console.log(i + ": " + this.answers[i]);
+//         }
+//     };
+
+//     Question.prototype.checkAnswer = function(ans) {
+//         if (ans === this.correct) {
+//             console.log("correct answer");
+//         } else {
+//             console.log("Wrong asnwer. Try again");
+//         }
+//     };
+
+//     var q1 = new Question("Is js coolest", ["Yes", "No"], 0);
+//     var q2 = new Question(
+//         "name of this course's instructor",
+//         ["John", "Michael", "jonas"],
+//         2,
+//     );
+//     var q3 = new Question(
+//         "What best describe coding",
+//         ["Boring", "Hard", "Fun", "tedious"],
+//         2,
+//     );
+
+//     var questions = [q1, q2, q3];
+//     var n = Math.floor(Math.random() * questions.length);
+//     questions[n].displayQuestion();
+//     var answer = parseInt(prompt("Please select the correct answer"));
+//     questions[n].checkAnswer(answer);
+// })();
+
 (function() {
     function Question(question, answers, correct) {
         this.question = question;
@@ -60,12 +101,21 @@
         }
     };
 
-    Question.prototype.checkAnswer = function(ans) {
+    Question.prototype.checkAnswer = function(ans, callback) {
+        var sc;
         if (ans === this.correct) {
             console.log("correct answer");
+            sc = callback(true);
         } else {
             console.log("Wrong asnwer. Try again");
+            sc = callback(false);
         }
+        this.displayScore(sc);
+    };
+
+    Question.prototype.displayScore = function(score) {
+        console.log("Your current score is: " + score);
+        console.log("------------------------------------");
     };
 
     var q1 = new Question("Is js coolest", ["Yes", "No"], 0);
@@ -79,10 +129,30 @@
         ["Boring", "Hard", "Fun", "tedious"],
         2,
     );
-
     var questions = [q1, q2, q3];
-    var n = Math.floor(Math.random() * questions.length);
-    questions[n].displayQuestion();
-    var answer = parseInt(prompt("Please select the correct answer"));
-    questions[n].checkAnswer(answer);
+
+    function score() {
+        var score = 0;
+        return function(correct) {
+            if (correct) {
+                score++;
+            }
+            return score;
+        };
+    }
+
+    var keepScore = score();
+
+    function nextQuestion() {
+        var n = Math.floor(Math.random() * questions.length);
+        questions[n].displayQuestion();
+        var answer = prompt("Please select the correct answer");
+
+        if (answer !== "exit") {
+            questions[n].checkAnswer(parseInt(answer), keepScore);
+            nextQuestion();
+        }
+    }
+
+    nextQuestion();
 })();
