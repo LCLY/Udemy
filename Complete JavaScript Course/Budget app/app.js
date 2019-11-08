2452434ed6c21400774f235d5d683dfb5c1fd647
LCLY <lchoo9294@gmail.com>
2019-11-08 17:35:53 +0800

    update: creating different controllers with public methods

--- Complete JavaScript Course/Budget app/app.js
+++ Complete JavaScript Course/Budget app/app.js
@@ -1,25 +1,52 @@
-var budgetController = (function() {
-    var x = 23;
-    var add = function(a) {
-        return x + a;
+/* ========== budget controller ========== */
+var budgetController = (function() {})();
+
+// Immediately Invoke Function Expression
+/* ========== UI controller ========== */
+var UIController = (function() {
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        inputBtn: ".add__btn",
     };
-    // this way the methods are private
-    //people can only usee the publicTest method instead of the add method, they wont have access to it
     return {
-        publicTest: function(b) {
-            return add(b); //because of closures, this method still have access to the variable in this function even though the outer function already return
+        getinput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //will getting either income or expenses
+                description: document.querySelector(DOMstrings.inputDescription)
+                    .value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            };
+        },
+
+        //now this is a public method for other controller to obtain the DOM strings
+        getDOMstrings: function() {
+            return DOMstrings;
         },
     };
 })();
 
-// Immediately Invoke Function Expression
-var UIController = (function() {})();
-
+/* ========== controller ========== */
 var controller = (function(budgetCtrl, UICtrl) {
-    var z = budgetCtrl.publicTest(5);
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        },
+    var DOM = UICtrl.getDOMstrings();
+    //to not repeat yourself
+    var ctrlAddItem = function() {
+        //1. Get the filled input data
+        var input = UICtrl.getinput();
+        console.log(input);
+
+        //2. Add item to the budget controller
+        //3. Add the new item to the UI
+        //4. calculate the budget
+        //5. display the budget
     };
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem); //we can make it a callback function here
+
+    document.addEventListener("keypress", function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            //which is for older browser
+            ctrlAddItem();
+        }
+    });
 })(budgetController, UIController);
