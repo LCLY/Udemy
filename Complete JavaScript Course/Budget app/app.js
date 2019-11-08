45a618c23e8b188a11b360147f5cebaaf5f52088
LCLY <lchoo9294@gmail.com>
2019-11-08 17:43:51 +0800

    refactor: added new method for initialization

--- Complete JavaScript Course/Budget app/app.js
+++ Complete JavaScript Course/Budget app/app.js
@@ -29,24 +29,37 @@ var UIController = (function() {
 
 /* ========== controller ========== */
 var controller = (function(budgetCtrl, UICtrl) {
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+        document
+            .querySelector(DOM.inputBtn)
+            .addEventListener("click", ctrlAddItem); //we can make it a callback function here
+
+        document.addEventListener("keypress", function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                //which is for older browser
+                ctrlAddItem();
+            }
+        });
+    };
+
     //to not repeat yourself
     var ctrlAddItem = function() {
         //1. Get the filled input data
         var input = UICtrl.getinput();
-        console.log(input);
 
         //2. Add item to the budget controller
         //3. Add the new item to the UI
         //4. calculate the budget
         //5. display the budget
     };
-    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem); //we can make it a callback function here
 
-    document.addEventListener("keypress", function(event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            //which is for older browser
-            ctrlAddItem();
-        }
-    });
+    return {
+        init: function() {
+            // initialization
+            setupEventListeners();
+        },
+    };
 })(budgetController, UIController);
+
+controller.init();
