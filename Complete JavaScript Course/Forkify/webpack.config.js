50a90a6f88d1c9177f50e026dd0825e68e274516
LCLY <lchoo9294@gmail.com>
2019-11-12 15:32:57 +0800

    built: setting up plugins

--- Complete JavaScript Course/Forkify/webpack.config.js
+++ Complete JavaScript Course/Forkify/webpack.config.js
@@ -1,12 +1,23 @@
 const path = require("path"); //absolute path
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     // entry point is where webpack start bundling
     entry: "./src/js/index.js",
     // where to save our bundle file
     output: {
-        path: path.resolve(__dirname, "dist/js"), //__dirname is current absolute path
-        // so we are joining the absolute path with the dir we want which is dist/js
-        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"), //__dirname is current absolute path
+        // so we are joining the absolute path with the dir we want which is dist
+        filename: "js/bundle.js",
     },
-    mode: "development", //there will be optimization if its production mode, minifying and tree shaking
+    // mode: "development", //there will be optimization if its production mode, minifying and tree shaking
+    devServer: {
+        contentBase: "./dist",
+    },
+    // It allows us to do complex processing of our input files and in this case its the index.html file
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./src/index.html",
+        }),
+    ],
 };
