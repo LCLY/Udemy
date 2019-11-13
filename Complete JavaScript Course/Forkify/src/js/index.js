4d7a31ee0e346c3d1bbba1579e2f2232cea80dde
LCLY <lchoo9294@gmail.com>
2019-11-13 23:46:44 +0800

    task: writing render controller

--- Complete JavaScript Course/Forkify/src/js/index.js
+++ Complete JavaScript Course/Forkify/src/js/index.js
@@ -1,4 +1,6 @@
 import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements } from "./views/base";
 // this is our main controller
 
 /* global state of the app
@@ -7,26 +9,28 @@ import Search from "./models/Search";
  * Shopping list object
  * Liked recipes
  */
+// just an object to store the state
 const state = {};
 
 const controlSearch = async () => {
-    // 1. get query from view
-    const query = "pizza"; //TODO
+	// 1. get query from view
+	const query = searchView.getInput(); //TODO
 
-    if (query) {
-        // 2. new search object and add to state
-        state.search = new Search(query);
-        // 3. prepare UI for results
+	if (query) {
+		// 2. new search object and add to state
+		state.search = new Search(query);
+		// 3. prepare UI for results
 
-        // 4. Search for recipes
-        await state.search.getResults();
+		// 4. Search for recipes
+		await state.search.getResults();
 
-        // 5. render the results to ui
-        console.log(state.search.result);
-    }
+		// 5. render the results to ui
+		// console.log(state.search.result);
+		searchView.renderResults(state.search.result);
+	}
 };
 
-document.querySelector(".search").addEventListener("submit", e => {
-    e.preventDefault();
-    controlSearch();
+elements.searchForm.addEventListener("submit", e => {
+	e.preventDefault();
+	controlSearch();
 });
---
+++ Complete JavaScript Course/Forkify/src/js/views/base.js
@@ -0,0 +1,6 @@
+// here is to store all the element's name
+export const elements = {
+	searchForm: document.querySelector(".search"),
+	searchInput: document.querySelector(".search__field"),
+	searchResultList: document.querySelector(".results__list")
+};
