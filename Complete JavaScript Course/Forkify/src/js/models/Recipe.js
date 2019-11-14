8bfe55030fc56c669c7ee7d6f92b94f22f67e5d7
LCLY <lchoo9294@gmail.com>
2019-11-15 02:44:22 +0800

    task: filter, parse the ingredients into the format we want

--- Complete JavaScript Course/Forkify/src/js/models/Recipe.js
+++ Complete JavaScript Course/Forkify/src/js/models/Recipe.js
@@ -34,4 +34,89 @@ export default class Recipe {
 	calcServings() {
 		this.servings = 4;
 	}
+
+	parseIngredients() {
+		const unitsLong = [
+			"tablespoons",
+			"tablespoon",
+			"ounces",
+			"ounce",
+			"teaspoons",
+			"teaspoon",
+			"cups",
+			"pounds"
+		];
+		const unitsShort = [
+			"tbsp",
+			"tbsp",
+			"oz",
+			"oz",
+			"tsp",
+			"tsp",
+			"cup",
+			"pound"
+		];
+		// for each ingredient element
+		const newIngredients = this.ingredients.map(el => {
+			// 1. uniform units
+			let ingredient = el.toLowerCase();
+			unitsLong.forEach((unit, i) => {
+				// unit is the current value, i is index
+				// for every ingredient replace the unit with the shorter unit
+				// they will be replace by the unitShort that has the same position as unitLong
+				ingredient = ingredient.replace(unit, unitsShort[i]);
+			});
+			// 2. remove parenthesis
+			ingredient = ingredient.replace(/ *\([^)]*\) */g, " ");
+
+			// 3. parse ingredients into count, unit and ingredient
+			const arrIng = ingredient.split(" ");
+			// it will return true if the element is inside the array
+			const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+
+			let objIng;
+			if (unitIndex > -1) {
+				// there is a unit
+				//Ex. 4 1/2 cups, arrCount is [4, 1/2]
+				//Ex. 4 cups, arrCount is [4]
+				const arrCount = arrIng.slice(0, unitIndex);
+				let count;
+				if (arrCount.length === 1) {
+					//if theres only 1 number before the unit then that number is the count itself
+					// also in the data, some unit is repesented as 1-1/2 which means 1 and a half
+					// so we replace - with + and eval to return a whole number
+					count = eval(arrIng[0].replace("-", "+"));
+				} else {
+					// using the example above, it will return "4+1/2"
+					// with eval("4+1/2") --> 4.5
+					count = eval(arrIng.slice(0, unitIndex).join("+"));
+				}
+
+				objIng = {
+					count,
+					unit: arrIng[unitIndex],
+					ingredient: arrIng.slice(unitIndex + 1).join(" ")
+				};
+			} else if (parseInt(arrIng[0], 10)) {
+				// There is no unit but 1st element is a number
+				// slice(1) -> is to start copying the array after the first element
+				objIng = {
+					count: parseInt(arrIng[0], 10),
+					unit: "",
+					ingredient: arrIng.slice(1).join(" ")
+				};
+			} else if (unitIndex === -1) {
+				// there is no unit and no number in first position
+				// ES6 -> instead of ingredient: ingredient just do ingredient
+				objIng = {
+					count: 1,
+					unit: "",
+					ingredient
+				};
+			}
+
+			return objIng; //each iteration we need to return to be saved to the current position of the new array
+		});
+		this.ingredients = newIngredients;
+	}
 }
