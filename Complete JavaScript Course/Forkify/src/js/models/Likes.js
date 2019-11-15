73bcd0bf20a99170c1c5f25b1575d5d3c0d3aed1
LCLY <lchoo9294@gmail.com>
2019-11-16 04:09:26 +0800

    task: persist data and read storage

--- Complete JavaScript Course/Forkify/src/js/models/Likes.js
+++ Complete JavaScript Course/Forkify/src/js/models/Likes.js
@@ -6,12 +6,18 @@ export default class Likes {
 	addLike(id, title, author, img) {
 		const like = { id, title, author, img };
 		this.likes.push(like);
+
+		// persist data in the localStorage
+		this.persistData();
 		return like;
 	}
 
 	deleteLike(id) {
 		const index = this.likes.findIndex(el => el.id === id);
+
+		// Persist data in the localStorage
 		this.likes.splice(index, 1);
+		this.persistData();
 	}
 
 	isLiked(id) {
@@ -21,5 +27,17 @@ export default class Likes {
 
 	getNumLikes() {
 		return this.likes.length;
+	}
+
+	persistData() {
+		localStorage.setItem("likes", JSON.stringify(this.likes));
+	}
+
+	readStorage() {
+		// parse converts data back from string to whatever it was
+		const storage = JSON.parse(localStorage.getItem("likes"));
+
+		// restoring likes from the localStorage
+		if (storage) this.likes = storage;
 	}
 }
