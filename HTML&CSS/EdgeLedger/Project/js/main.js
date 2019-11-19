d5e0b5b82e714e302dfcb0c149a48adaea832a2d
LCLY <lchoo9294@gmail.com>
2019-11-20 00:54:31 +0800

    refactor: added smooth scrolling

--- HTML&CSS/EdgeLedger/Project/js/main.js
+++ HTML&CSS/EdgeLedger/Project/js/main.js
@@ -10,3 +10,17 @@ function initMap() {
 	// The marker, positioned at location
 	const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+// Smooth scrolling
+$("#navbar a, .btn").on("click", function(event) {
+	if (this.hash !== "") {
+		event.preventDefault();
+		const hash = this.hash;
+		$("html, body").animate(
+			{
+				scrollTop: $(hash).offset().top - 100
+			},
+			800
+		);
+	}
+});
