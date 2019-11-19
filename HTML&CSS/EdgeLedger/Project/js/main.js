a314bcf7f177939f353abc9341a4aca6f5d06faf
LCLY <lchoo9294@gmail.com>
2019-11-20 00:37:29 +0800

    refactor: created script for google map

---
+++ HTML&CSS/EdgeLedger/Project/js/main.js
@@ -0,0 +1,12 @@
+function initMap() {
+	// your location
+	const loc = { lat: 42.361145, lng: -71.057083 };
+	// centered map on location
+	const map = new google.maps.Map(document.querySelector(".map"), {
+		zoom: 14,
+		center: loc
+	});
+
+	// The marker, positioned at location
+	const marker = new google.maps.Marker({ position: loc, map: map });
+}
