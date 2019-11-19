f4b002326da97e811441322b71791e82b26cf447
LCLY <lchoo9294@gmail.com>
2019-11-20 02:48:07 +0800

    fix: google map issue

--- HTML&CSS/EdgeLedger/Project/js/main.js
+++ HTML&CSS/EdgeLedger/Project/js/main.js
@@ -1,15 +1,15 @@
-function initMap() {
-	// your location
-	const loc = { lat: 42.361145, lng: -71.057083 };
-	// centered map on location
-	const map = new google.maps.Map(document.querySelector(".map"), {
-		zoom: 14,
-		center: loc
-	});
+// function initMap() {
+// 	// your location
+// 	const loc = { lat: 42.361145, lng: -71.057083 };
+// 	// centered map on location
+// 	const map = new google.maps.Map(document.querySelector(".map"), {
+// 		zoom: 14,
+// 		center: loc
+// 	});
 
-	// The marker, positioned at location
-	const marker = new google.maps.Marker({ position: loc, map: map });
-}
+// 	// The marker, positioned at location
+// 	const marker = new google.maps.Marker({ position: loc, map: map });
+// }
 
 // Sticky menu background
 window.addEventListener("scroll", function() {
