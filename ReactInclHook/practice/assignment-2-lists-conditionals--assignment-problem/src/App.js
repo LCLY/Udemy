4ec691cb5baf692b5d4e4478b4655a50f563e3a4
LCLY <lchoo9294@gmail.com>
2019-11-25 16:26:49 +0800

    important: styled components

--- ReactInclHook/practice/assignment-2-lists-conditionals--assignment-problem/src/App.js
+++ ReactInclHook/practice/assignment-2-lists-conditionals--assignment-problem/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import Validation from "./Components/ValidationComponent";
 import CharComponent from "./Components/CharComponent";
-import Styled from "styled-components";
+import styled from "styled-components";
 import "./App.css";
 
-const StyledDiv = Styled.div`
-	backgroundcolor: red;
+const StyledDiv = styled.div`
+	background: blue;
+	&:hover {
+		background-color: red;
+	}
 `;
 
 class App extends Component {
@@ -50,46 +53,50 @@ class App extends Component {
 
 		return (
 			<div className="App">
-				<ol>
-					<li>
-						Create an input field (in App component) with a change listener
-						which outputs the length of the entered text below it (e.g. in a
-						paragraph).
-					</li>
-					<li>
-						Create a new component (=> ValidationComponent) which receives the
-						text length as a prop
-					</li>
-					<li>
-						Inside the ValidationComponent, either output "Text too short" or
-						"Text long enough" depending on the text length (e.g. take 5 as a
-						minimum length)
-					</li>
-					<li>
-						Create another component (=> CharComponent) and style it as an
-						inline box (=> display: inline-block, padding: 16px, text-align:
-						center, margin: 16px, border: 1px solid black).
-					</li>
-					<li>
-						Render a list of CharComponents where each CharComponent receives a
-						different letter of the entered text (in the initial input field) as
-						a prop.
-					</li>
-					<li>
-						When you click a CharComponent, it should be removed from the
-						entered text.
-					</li>
-				</ol>
-				<p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
-				<input
-					type="text"
-					onChange={e => getLength(e)}
-					value={this.state.input}
-				></input>
-				<StyledDiv>TEST</StyledDiv>
-				<p>text length: {this.state.input.length}</p>
-				<Validation inputLength={this.state.input.length} />
-				{charList}
+				<StyledDiv>
+					<ol>
+						<li>
+							Create an input field (in App component) with a change listener
+							which outputs the length of the entered text below it (e.g. in a
+							paragraph).
+						</li>
+						<li>
+							Create a new component (=> ValidationComponent) which receives the
+							text length as a prop
+						</li>
+						<li>
+							Inside the ValidationComponent, either output "Text too short" or
+							"Text long enough" depending on the text length (e.g. take 5 as a
+							minimum length)
+						</li>
+						<li>
+							Create another component (=> CharComponent) and style it as an
+							inline box (=> display: inline-block, padding: 16px, text-align:
+							center, margin: 16px, border: 1px solid black).
+						</li>
+						<li>
+							Render a list of CharComponents where each CharComponent receives
+							a different letter of the entered text (in the initial input
+							field) as a prop.
+						</li>
+						<li>
+							When you click a CharComponent, it should be removed from the
+							entered text.
+						</li>
+					</ol>
+					<p>
+						Hint: Keep in mind that JavaScript strings are basically arrays!
+					</p>
+					<input
+						type="text"
+						onChange={e => getLength(e)}
+						value={this.state.input}
+					></input>
+					Anything inside
+					<p>text length: {this.state.input.length}</p>
+					<Validation inputLength={this.state.input.length} />
+					{charList}
+				</StyledDiv>
 			</div>
 		);
 	}
