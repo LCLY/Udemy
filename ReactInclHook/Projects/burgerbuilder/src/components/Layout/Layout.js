de295cfdff4ec6992529a62f21208369f335456b
LCLY <lchoo9294@gmail.com>
2019-11-26 16:45:21 +0800

    fix: change aux to <> import dir

--- ReactInclHook/Projects/burgerbuilder/src/components/Layout/Layout.js
+++ ReactInclHook/Projects/burgerbuilder/src/components/Layout/Layout.js
@@ -1,8 +1,12 @@
 import React from "react";
-import Aux from "../../hoc/Auxiliary";
-const Layout = props => (
-	<Aux>
-		<div>Toolbar, SideDrawer, Backdrop</div>
-		<main>{props.children}</main>
-	</Aux>
-);
+
+const Layout = props => {
+	return (
+		<>
+			<div>Toolbar, SideDrawer, Backdrop</div>
+			<main>{props.children}</main>
+		</>
+	);
+};
+
+export default Layout;
