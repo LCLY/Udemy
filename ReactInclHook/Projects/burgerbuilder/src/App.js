0655fcff5303b2ad5ce3d518eca50cadaca66dae
LCLY <lchoo9294@gmail.com>
2019-11-26 16:19:45 +0800

    refactor: creating files

--- ReactInclHook/Projects/burgerbuilder/src/App.js
+++ ReactInclHook/Projects/burgerbuilder/src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import Layout from "./components/Layout";
 
 class App extends Component {
 	render() {
-		return <div>App</div>;
+		return (
+			<div>
+				<Layout>
+					<p>TESTING</p>
+				</Layout>
+			</div>
+		);
 	}
 }
 
---
+++ ReactInclHook/Projects/burgerbuilder/src/components/Layout/Layout.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Aux from "../../hoc/Auxiliary";
+const Layout = props => (
+	<Aux>
+		<div>Toolbar, SideDrawer, Backdrop</div>
+		<main>{props.children}</main>
+	</Aux>
+);
