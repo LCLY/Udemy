218579e1464804653f45e6d1cf3bf99f2a803bf3
LCLY <lchoo9294@gmail.com>
2019-11-27 12:42:19 +0800

    task: checking if ingredients exist

--- ReactInclHook/Projects/burgerbuilder/src/components/Burger/Burger.js
+++ ReactInclHook/Projects/burgerbuilder/src/components/Burger/Burger.js
@@ -2,20 +2,30 @@ import React from "react";
 import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const Burger = props => {
-	const transformedIngredients = Object.keys(props.ingredients).map(ingKey => {
-		// console.log("ingredients:", props.ingredients);
-		// console.log("array:", [...Array(props.ingredients[ingKey])]);
+	let transformedIngredients = Object.keys(props.ingredients)
+		.map(ingKey => {
+			// console.log("ingredients:", props.ingredients);
+			// console.log("array:", [...Array(props.ingredients[ingKey])]);
 
-		// we are creating new arrays here with the Array func
-		// we are retrieving the number that is stored in the ingredient key
-		// with that, we are creating new Arrays with the length of that number, and
-		// within that new array we render burgerIngredient
-		return [...Array(props.ingredients[ingKey])].map((_, i) => {
-			return (
-				<BurgerIngredient key={ingKey + i} type={ingKey}></BurgerIngredient>
-			);
-		});
-	});
+			// we are creating new arrays here with the Array func
+			// we are retrieving the number that is stored in the ingredient key
+			// with that, we are creating new Arrays with the length of that number, and
+			// within that new array we render burgerIngredient
+			return [...Array(props.ingredients[ingKey])].map((_, i) => {
+				return (
+					<BurgerIngredient key={ingKey + i} type={ingKey}></BurgerIngredient>
+				);
+			});
+			// we want to check if theres ingredient or not, one way is to use reduce
+			// we dont check the ingredients one by one, we can just compressed them
+			// into a single array with all the elements, if theres nothing, it will just be 0
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el);
+		}, []);
+	if (transformedIngredients.length === 0) {
+		transformedIngredients = <p>Please start adding ingredients</p>;
+	}
 
 	console.log("transformed:", transformedIngredients);
 	return (
