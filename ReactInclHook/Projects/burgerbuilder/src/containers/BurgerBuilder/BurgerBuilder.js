45d705f48af196a70abfd4d5cd37f8aab17c7355
LCLY <lchoo9294@gmail.com>
2019-11-27 13:18:27 +0800

    fix: updated Count canot hold oldCount++

--- ReactInclHook/Projects/burgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ ReactInclHook/Projects/burgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+const INGREDIENT_PRICES = {
+	salad: 0.5,
+	cheese: 0.4,
+	meat: 1.3,
+	bacon: 0.7
+};
+
 class BurgerBuilder extends Component {
 	state = {
 		ingredients: {
@@ -8,13 +15,31 @@ class BurgerBuilder extends Component {
 			bacon: 1,
 			cheese: 2,
 			meat: 2
-		}
+		},
+		totalPrice: 4
 	};
+
+	addIngredientHandler = type => {
+		const oldCount = this.state.ingredients[type];
+		const updatedCount = oldCount + 1;
+		// make a copy of the object
+		const updatedIngredients = {
+			...this.state.ingredients
+		};
+		// then replace the type's count with the new count
+		updatedIngredients[type] = updatedCount;
+		let priceAddition = INGREDIENT_PRICES[type];
+		let oldPrice = this.state.totalPrice;
+		let newPrice = oldPrice + priceAddition;
+		this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+	};
+
+	removeIngredientHandler = type => {};
 	render() {
 		return (
 			<>
 				<Burger ingredients={this.state.ingredients} />
-				<BuildControls />
+				<BuildControls ingredientAdded={this.addIngredientHandler} />
 			</>
 		);
 	}
