7af47c642b075941317fd283d20d9d2ff5f47592
LCLY <lchoo9294@gmail.com>
2019-11-27 16:54:41 +0800

    task: printing total price at checkout

--- ReactInclHook/Projects/burgerbuilder/src/components/Burger/OrderSummary/OrderSummary.js
+++ ReactInclHook/Projects/burgerbuilder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "../../UI/Button/Button";
 const OrderSummary = props => {
+	console.log("the price:", props.price);
 	const ingredientSummary = Object.keys(props.ingredients).map(ingKey => {
 		return (
 			<li key={ingKey}>
@@ -15,6 +16,9 @@ const OrderSummary = props => {
 			<p>A delicious burger with the following ingredients:</p>
 			<ul>{ingredientSummary}</ul>
 			<p>Continue to Checkout?</p>
+			<p>
+				<strong>Total Price: {props.price.toFixed(2)}</strong>
+			</p>
 			<Button btnType="Danger" clicked={props.purchaseCancelled}>
 				CANCEL
 			</Button>
