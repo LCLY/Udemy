0b3af2cf12d313dae2cfd2f0b46095c5cce94237
LCLY <lchoo9294@gmail.com>
2019-11-27 13:34:58 +0800

    fix: start with 0 ingredients

--- ReactInclHook/Projects/burgerbuilder/src/components/Burger/BuildControls/BuildControls.js
+++ ReactInclHook/Projects/burgerbuilder/src/components/Burger/BuildControls/BuildControls.js
@@ -10,6 +10,9 @@ const BuildControls = props => {
 	];
 	return (
 		<div className={classes.BuildControls}>
+			<p>
+				Current Price: <strong>{props.price.toFixed(2)}</strong>
+			</p>
 			{controls.map(el => {
 				return (
 					<BuildControl
