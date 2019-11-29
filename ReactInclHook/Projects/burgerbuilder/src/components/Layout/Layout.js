37f9a74dfa8ffaf263fed79883e7b316bee7f7f8
LCLY <lchoo9294@gmail.com>
2019-11-29 09:26:45 +0800

    task: layout toolbar sidedrawer

--- ReactInclHook/Projects/burgerbuilder/src/components/Layout/Layout.js
+++ ReactInclHook/Projects/burgerbuilder/src/components/Layout/Layout.js
@@ -1,14 +1,13 @@
 import React from "react";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
-import 
-<SideDrawer/> from "../Navigation/SideDrawer/SideDrawer";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const Layout = props => {
 	return (
 		<>
 			<Toolbar />
-			<SideDrawer/>
+			<SideDrawer />
 			<main className={classes.Content}>{props.children}</main>
 		</>
 	);
