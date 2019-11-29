9427d7aede8cffa813c06ea87d69611de8d03b13
LCLY <lchoo9294@gmail.com>
2019-11-29 10:57:45 +0800

    refactor: convert to to classComponent

--- ReactInclHook/Projects/burgerbuilder/src/components/Burger/OrderSummary/OrderSummary.js
+++ ReactInclHook/Projects/burgerbuilder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,36 @@
 import React from "react";
 import Button from "../../UI/Button/Button";
-const OrderSummary = props => {
-	console.log("the price:", props.price);
-	const ingredientSummary = Object.keys(props.ingredients).map(ingKey => {
-		return (
-			<li key={ingKey}>
-				<span style={{ textTransform: "capitalize" }}>{ingKey}</span>:&nbsp;
-				{props.ingredients[ingKey]}
-			</li>
+class OrderSummary extends React.Component {
+	render() {
+		console.log("the price:", this.props.price);
+		const ingredientSummary = Object.keys(this.props.ingredients).map(
+			ingKey => {
+				return (
+					<li key={ingKey}>
+						<span style={{ textTransform: "capitalize" }}>{ingKey}</span>:&nbsp;
+						{this.props.ingredients[ingKey]}
+					</li>
+				);
+			}
 		);
-	});
-	return (
-		<>
-			<h3>Your Order</h3>
-			<p>A delicious burger with the following ingredients:</p>
-			<ul>{ingredientSummary}</ul>
-			<p>Continue to Checkout?</p>
-			<p>
-				<strong>Total Price: {props.price.toFixed(2)}</strong>
-			</p>
-			<Button btnType="Danger" clicked={props.purchaseCancelled}>
-				CANCEL
-			</Button>
-			<Button btnType="Success" clicked={props.purchaseContinued}>
-				CONTINUE
-			</Button>
-		</>
-	);
-};
+		return (
+			<>
+				<h3>Your Order</h3>
+				<p>A delicious burger with the following ingredients:</p>
+				<ul>{ingredientSummary}</ul>
+				<p>Continue to Checkout?</p>
+				<p>
+					<strong>Total Price: {this.props.price.toFixed(2)}</strong>
+				</p>
+				<Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+					CANCEL
+				</Button>
+				<Button btnType="Success" clicked={this.props.purchaseContinued}>
+					CONTINUE
+				</Button>
+			</>
+		);
+	}
+}
 
 export default OrderSummary;
