7aead875465e8fdac51bf7007ee8e26dafb93c62
LCLY <lchoo9294@gmail.com>
2019-11-29 16:42:18 +0800

    important: retreive data with id

--- ReactInclHook/Projects/httpReact/src/components/FullPost/FullPost.js
+++ ReactInclHook/Projects/httpReact/src/components/FullPost/FullPost.js
@@ -1,22 +1,47 @@
-import React, { Component } from 'react';
-
-import './FullPost.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./FullPost.css";
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+	state = {
+		loadedPost: null
+	};
+	componentDidUpdate() {
+		if (this.props.id) {
+			// check if theres no existing loadedPost then fetch one first to fill up the empty state
+			// only do a new get if the current selected post is different than the previous one that was stored in loadedPost
+			if (
+				!this.state.loadedPost ||
+				(this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+			) {
+				axios
+					.get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+					.then(res => {
+						this.setState({ loadedPost: res.data });
+					});
+			}
+		}
+	}
+	render() {
+		let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+		if (this.props.id) {
+			post = <p style={{ textAlign: "center" }}>Loading...</p>;
+		}
+		// only render if loadedPost has data
+		if (this.state.loadedPost) {
+			post = (
+				<div className="FullPost">
+					<h1>{this.state.loadedPost.title}</h1>
+					<p>{this.state.loadedPost.content}</p>
+					<div className="Edit">
+						<button className="Delete">Delete</button>
+					</div>
+				</div>
+			);
+		}
 
-        );
-        return post;
-    }
+		return post;
+	}
 }
 
 export default FullPost;
--- ReactInclHook/Projects/httpReact/src/containers/Blog/Blog.js
+++ ReactInclHook/Projects/httpReact/src/containers/Blog/Blog.js
@@ -7,7 +7,8 @@ import "./Blog.css";
 
 class Blog extends Component {
 	state = {
-		posts: []
+		posts: [],
+		selectedPostId: null
 	};
 	componentDidMount() {
 		axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
@@ -21,16 +22,27 @@ class Blog extends Component {
 			this.setState({ posts: updatedPosts });
 		});
 	}
+
+	postSelectedhandler = id => {
+		this.setState({ selectedPostId: id });
+	};
 	render() {
 		const posts = this.state.posts.map(post => {
 			console.log(post.title);
-			return <Post key={post.id} title={post.title} author={post.author} />;
+			return (
+				<Post
+					key={post.id}
+					title={post.title}
+					author={post.author}
+					clicked={() => this.postSelectedhandler(post.div)}
+				/>
+			);
 		});
 		return (
 			<div>
 				<section className="Posts">{posts}</section>
 				<section>
-					<FullPost />
+					<FullPost id={this.state.selectedPostId} />
 				</section>
 				<section>
 					<NewPost />
