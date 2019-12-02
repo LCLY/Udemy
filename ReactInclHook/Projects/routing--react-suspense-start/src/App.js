22b179a04991bd509092055f61bbcb5314e62458
LCLY <lchoo9294@gmail.com>
2019-12-03 03:24:42 +0800

    important: react suspense

--- ReactInclHook/Projects/routing--react-suspense-start/src/App.js
+++ ReactInclHook/Projects/routing--react-suspense-start/src/App.js
@@ -1,26 +1,37 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import React, { Component, Suspense } from "react";
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
 
-import Posts from './containers/Posts';
-import User from './containers/User';
-import Welcome from './containers/Welcome';
+// if we import like this it will always rendered in advance
+// import Posts from "./containers/Posts";
+import User from "./containers/User";
+import Welcome from "./containers/Welcome";
+
+const Posts = React.lazy(() => import("./containers/Posts"));
 
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <React.Fragment>
-          <nav>
-            <NavLink to="/user">User Page</NavLink> |&nbsp;
-            <NavLink to="/posts">Posts Page</NavLink>
-          </nav>
-          <Route path="/" component={Welcome} exact />
-          <Route path="/user" component={User} />
-          <Route path="/posts" component={Posts} />
-        </React.Fragment>
-      </BrowserRouter>
-    );
-  }
+	render() {
+		return (
+			<BrowserRouter>
+				<React.Fragment>
+					<nav>
+						<NavLink to="/user">User Page</NavLink> |&nbsp;
+						<NavLink to="/posts">Posts Page</NavLink>
+					</nav>
+					<Route path="/" component={Welcome} exact />
+					<Route path="/user" component={User} />
+					<Route
+						path="/posts"
+						render={() => (
+							// this fallback is when waiting for post to load
+							<Suspense fallback={<div>loading...</div>}>
+								<Posts />
+							</Suspense>
+						)}
+					/>
+				</React.Fragment>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
