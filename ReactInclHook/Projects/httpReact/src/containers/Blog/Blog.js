85b1eaa8b8eb0228180c24d76b389e4f9737c416
LCLY <lchoo9294@gmail.com>
2019-12-02 21:46:16 +0800

    refactor: added spinner to loading blog posts

--- ReactInclHook/Projects/httpReact/src/containers/Blog/Blog.js
+++ ReactInclHook/Projects/httpReact/src/containers/Blog/Blog.js
@@ -3,18 +3,21 @@ import axios from "../../axios";
 import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
 import NewPost from "../../components/NewPost/NewPost";
+import Spinner from "../../components/UI/Spinner/Spinner";
 import "./Blog.css";
 
 class Blog extends Component {
 	state = {
 		posts: [],
 		selectedPostId: null,
-		error: false
+		error: false,
+		loading: true
 	};
 	componentDidMount() {
 		axios
 			.get("/posts")
 			.then(response => {
+				this.setState({ loading: false });
 				const posts = response.data.slice(0, 4);
 				const updatedPosts = posts.map(post => {
 					return {
@@ -50,8 +53,25 @@ class Blog extends Component {
 		}
 
 		return (
-			<div>
-				<section className="Posts">{posts}</section>
+			<div className="Blog">
+				<header>
+					<nav>
+						<ul>
+							<li>
+								<a href="/">Home</a>
+							</li>
+							<li>
+								<a href="/new-post">New Post</a>
+							</li>
+						</ul>
+					</nav>
+				</header>
+				{this.state.loading ? (
+					<Spinner />
+				) : (
+					<section className="Posts">{posts}</section>
+				)}
+
 				<section>
 					<FullPost id={this.state.selectedPostId} />
 				</section>
