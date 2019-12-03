e6097b21b24ede6e8688c79494f4a4970d938229
LCLY <lchoo9294@gmail.com>
2019-12-03 21:11:54 +0800

    important: extract price and pass down to contact data

--- ReactInclHook/Projects/burgerbuilder/src/containers/Checkout/Checkout.js
+++ ReactInclHook/Projects/burgerbuilder/src/containers/Checkout/Checkout.js
@@ -4,22 +4,23 @@ import { Route } from "react-router-dom";
 import ContactData from "../Checkout/ContactData/ContactData";
 class Checkout extends Component {
 	state = {
-		ingredients: {
-			salad: 1,
-			meat: 1,
-			cheese: 1,
-			bacon: 1
-		}
+		ingredients: null,
+		totalPrice: 0
 	};
 
 	componentDidMount() {
 		const query = new URLSearchParams(this.props.location.search);
 		const ingredients = {};
+		let price = 0;
 		for (let param of query.entries()) {
 			// ['salad', '1']
-			ingredients[param[0]] = +param[1];
+			if (param[0] === "price") {
+				price = param[1]; //extract the value
+			} else {
+				ingredients[param[0]] = +param[1];
+			}
 		}
-		this.setState({ ingredients: ingredients });
+		this.setState({ ingredients: ingredients, totalPrice: price });
 	}
 
 	checkoutCancelledHandler = () => {
@@ -39,7 +40,12 @@ class Checkout extends Component {
 				/>
 				<Route
 					path={this.props.match.path + "/contact-data"}
-					component={ContactData}
+					render={() => (
+						<ContactData
+							ingredients={this.state.ingredients}
+							price={this.state.totalPrice}
+						/>
+					)}
 				/>
 			</div>
 		);
