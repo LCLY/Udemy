b190810a40ae88294580f8e4bb0d615f62753aeb
LCLY <lchoo9294@gmail.com>
2019-12-03 21:13:55 +0800

    important: change did mount to will mount because did mount no data yet

--- ReactInclHook/Projects/burgerbuilder/src/containers/Checkout/ContactData/ContactData.js
+++ ReactInclHook/Projects/burgerbuilder/src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Button from "../../../components/UI/Button/Button";
 import classes from "./ContactData.module.css";
+import axios from "../../../axios-orders";
 class ContactData extends Component {
 	state = {
 		name: "",
