b29b6c3905fd3fdf865b80d449ba17a022bf953a
LCLY <lchoo9294@gmail.com>
2019-12-03 21:32:34 +0800

    task: adding spinner to form

--- ReactInclHook/Projects/burgerbuilder/src/containers/Checkout/ContactData/ContactData.js
+++ ReactInclHook/Projects/burgerbuilder/src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Button from "../../../components/UI/Button/Button";
 import classes from "./ContactData.module.css";
+import Spinner from "../../../components/UI/Spinner/Spinner";
 import axios from "../../../axios-orders";
 class ContactData extends Component {
 	state = {
@@ -35,47 +36,66 @@ class ContactData extends Component {
 			.post("/orders.json", order)
 			.then(res => {
 				console.log(res);
-				this.setState({ loading: false, purchasing: false });
+				this.setState({ loading: false });
+				// we will need to pass props into the render in checkout.js in order to obtain the props history
+				// 	<Route
+				// 	path={this.props.match.path + "/contact-data"}
+				// 	render={props => (
+				// 		<ContactData
+				// 			ingredients={this.state.ingredients}
+				// 			price={this.state.totalPrice}
+				// 			{...props}
+				// 		/>
+				// 	)}
+				// />
+				this.props.history.push("/"); //redirect to homepage
 			})
 			.catch(err => {
 				console.log(err);
-				this.setState({ loading: false, purchasing: false });
+				this.setState({ loading: false });
 			});
 	};
 
 	render() {
+		let form = (
+			<form>
+				<input
+					className={classes.Input}
+					type="text"
+					name="name"
+					placeholder="Your name"
+				/>
+				<input
+					className={classes.Input}
+					type="email"
+					name="email"
+					placeholder="Your email"
+				/>
+				<input
+					className={classes.Input}
+					type="text"
+					name="street"
+					placeholder="Street"
+				/>
+				<input
+					className={classes.Input}
+					type="text"
+					name="postal"
+					placeholder="Postal Code"
+				/>
+				<Button btnType="Success" clicked={this.orderHandler}>
+					ORDER
+				</Button>
+			</form>
+		);
+
+		if (this.state.loading) {
+			form = <Spinner />;
+		}
 		return (
 			<div className={classes.ContactData}>
 				<h4>Enter Your Contact Data</h4>
-				<form>
-					<input
-						className={classes.Input}
-						type="text"
-						name="name"
-						placeholder="Your name"
-					/>
-					<input
-						className={classes.Input}
-						type="email"
-						name="email"
-						placeholder="Your email"
-					/>
-					<input
-						className={classes.Input}
-						type="text"
-						name="street"
-						placeholder="Street"
-					/>
-					<input
-						className={classes.Input}
-						type="text"
-						name="postal"
-						placeholder="Postal Code"
-					/>
-					<Button btnType="Success" clicked={this.orderHandler}>
-						ORDER
-					</Button>
-				</form>
+				{form}
 			</div>
 		);
 	}
