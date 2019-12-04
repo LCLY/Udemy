b46062d0b51a418aab17544aabd2d07271a847d9
LCLY <lchoo9294@gmail.com>
2019-12-05 02:01:28 +0800

    important: setting up react redux

--- ReactInclHook/practice/redux--01-start/src/containers/Counter/Counter.js
+++ ReactInclHook/practice/redux--01-start/src/containers/Counter/Counter.js
@@ -1,41 +1,71 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import CounterControl from '../../components/CounterControl/CounterControl';
-import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import { connect } from "react-redux";
+
+import CounterControl from "../../components/CounterControl/CounterControl";
+import CounterOutput from "../../components/CounterOutput/CounterOutput";
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
+	state = {
+		counter: 0
+	};
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
+	counterChangedHandler = (action, value) => {
+		switch (action) {
+			case "inc":
+				this.setState(prevState => {
+					return { counter: prevState.counter + 1 };
+				});
+				break;
+			case "dec":
+				this.setState(prevState => {
+					return { counter: prevState.counter - 1 };
+				});
+				break;
+			case "add":
+				this.setState(prevState => {
+					return { counter: prevState.counter + value };
+				});
+				break;
+			case "sub":
+				this.setState(prevState => {
+					return { counter: prevState.counter - value };
+				});
+				break;
+		}
+	};
 
-    render () {
-        return (
-            <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<CounterOutput value={this.props.ctr} />
+				<CounterControl
+					label="Increment"
+					clicked={() => this.counterChangedHandler("inc")}
+				/>
+				<CounterControl
+					label="Decrement"
+					clicked={() => this.counterChangedHandler("dec")}
+				/>
+				<CounterControl
+					label="Add 5"
+					clicked={() => this.counterChangedHandler("add", 5)}
+				/>
+				<CounterControl
+					label="Subtract 5"
+					clicked={() => this.counterChangedHandler("sub", 5)}
+				/>
+			</div>
+		);
+	}
 }
 
-export default Counter;
+// to get the redux state and convert it into the form of props
+const mapStateToProps = state => {
+	return {
+		// this is from the initalState in reducer
+		ctr: state.counter
+	};
+};
+
+export default connect(mapStateToProps)(Counter);
