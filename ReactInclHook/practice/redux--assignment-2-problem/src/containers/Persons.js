ff3d71c510f46ad3ca5df17d92caa121c9ee617f
LCLY <lchoo9294@gmail.com>
2019-12-05 18:01:50 +0800

    important: redux practice add and remove items completed

--- ReactInclHook/practice/redux--assignment-2-problem/src/containers/Persons.js
+++ ReactInclHook/practice/redux--assignment-2-problem/src/containers/Persons.js
@@ -1,44 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Person from '../components/Person/Person';
-import AddPerson from '../components/AddPerson/AddPerson';
+import Person from "../components/Person/Person";
+import AddPerson from "../components/AddPerson/AddPerson";
+import * as actionTypes from "../store/actions";
 
+// react-redux
+import { connect } from "react-redux";
 class Persons extends Component {
-    state = {
-        persons: []
-    }
+	state = {
+		persons: []
+	};
 
-    personAddedHandler = () => {
-        const newPerson = {
-            id: Math.random(), // not really unique but good enough here!
-            name: 'Max',
-            age: Math.floor( Math.random() * 40 )
-        }
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.concat(newPerson)}
-        } );
-    }
+	personAddedHandler = () => {
+		const newPerson = {
+			id: Math.random(), // not really unique but good enough here!
+			name: "Max",
+			age: Math.floor(Math.random() * 40)
+		};
+		this.setState(prevState => {
+			return { persons: prevState.persons.concat(newPerson) };
+		});
+	};
 
-    personDeletedHandler = (personId) => {
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.filter(person => person.id !== personId)}
-        } );
-    }
+	personDeletedHandler = personId => {
+		this.setState(prevState => {
+			return {
+				persons: prevState.persons.filter(person => person.id !== personId)
+			};
+		});
+	};
 
-    render () {
-        return (
-            <div>
-                <AddPerson personAdded={this.personAddedHandler} />
-                {this.state.persons.map(person => (
-                    <Person 
-                        key={person.id}
-                        name={person.name} 
-                        age={person.age} 
-                        clicked={() => this.personDeletedHandler(person.id)}/>
-                ))}
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<AddPerson personAdded={this.props.onAddPersons} />
+				{this.props.persons.map(person => (
+					<Person
+						key={person.id}
+						name={person.name}
+						age={person.age}
+						clicked={() => this.props.onDeletePersons(person.id)}
+					/>
+				))}
+			</div>
+		);
+	}
 }
+const mapStateToProps = state => {
+	return {
+		persons: state.personReducer.persons
+	};
+};
 
-export default Persons;
+const mapDispatchToProps = dispatch => {
+	return {
+		onAddPersons: () => dispatch({ type: actionTypes.ADD_PERSON }),
+		// pass in id here
+		onDeletePersons: id =>
+			dispatch({ type: actionTypes.DELETE_PERSON, personId: id })
+	};
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
--- ReactInclHook/practice/redux--assignment-2-problem/src/index.js
+++ ReactInclHook/practice/redux--assignment-2-problem/src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// react-redux
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import personReducer from "./store/reducers/person";
+
+const rootReducer = combineReducers({ personReducer: personReducer });
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
 registerServiceWorker();
