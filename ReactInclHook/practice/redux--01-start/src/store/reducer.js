0e1e53464c4ac7c8c1182bacced0454ee07add50
LCLY <lchoo9294@gmail.com>
2019-12-05 13:50:49 +0800

    important: deleting objects with id and redux and filter

--- ReactInclHook/practice/redux--01-start/src/store/reducer.js
+++ ReactInclHook/practice/redux--01-start/src/store/reducer.js
@@ -18,6 +18,29 @@ const reducer = (state = initialState, action) => {
 				...state,
 				results: state.results.concat({ id: new Date(), value: state.counter })
 			};
+		case "DELETE_RESULT":
+			// delete array immutably
+			// const newArray = [...state.result];
+			// newArray.splice(id, 1);
+
+			// filter returns a new array
+			// it takes function as input, the function is executed on each element in the array
+			// it determines whether this element fulfils a certain condition to make it into
+			// the new array which is returned by filter or not
+
+			// *** to use filter to delete element, we return true for every element that doesnt
+			// have a certain ID or which is not a certain index here
+			const updatedArray = state.results.filter(
+				// return true if that index is not equal to the index of element that we want to remove it at
+				// (result, index) => index !== id
+				// OR
+				// instead of using the index, we can use the id that we set in the state object
+				result => result.id !== action.resultElementId
+			);
+			return {
+				...state,
+				results: updatedArray
+			};
 	}
 
 	// if (action.type === "INCREMENT") {
