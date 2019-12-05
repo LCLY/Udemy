81da49f5f47cce28fb6bfb7277628262a2c317db
LCLY <lchoo9294@gmail.com>
2019-12-06 02:35:35 +0800

    refactor: split up the actions

---
+++ ReactInclHook/practice/redux--01-start/src/store/actions/result.js
@@ -0,0 +1,22 @@
+import * as actionTypes from "./actionTypes";
+
+export const saveResult = res => {
+	return {
+		type: actionTypes.STORE_RESULT,
+		result: res
+	};
+};
+export const storeResult = res => {
+	// we can do this bcoz of redux thunk
+	return dispatch => {
+		setTimeout(() => {
+			dispatch(saveResult(res));
+		}, 2000);
+	};
+};
+export const deleteResult = resElId => {
+	return {
+		type: actionTypes.DELETE_RESULT,
+		resultElementId: resElId
+	};
+};
