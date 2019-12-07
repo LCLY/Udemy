138cdb671680b67942097dd4647d839baaf0da7e
LCLY <lchoo9294@gmail.com>
2019-12-08 01:41:59 +0800

    fix: route issues

--- ReactInclHook/Projects/burgerbuilder/src/App.js
+++ ReactInclHook/Projects/burgerbuilder/src/App.js
@@ -29,7 +29,8 @@ class App extends Component {
 				<Switch>
 					<Route path="/checkout" component={Checkout} />
 					<Route path="/orders" component={Orders} />
-					<Route path="/logout" component={Logout} />
+					<Route path="/logout" component={Logout} />{" "}
+					<Route path="/auth" component={Auth} />
 					<Route path="/" exact component={BurgerBuilder} />
 					<Redirect to="/" />
 				</Switch>
