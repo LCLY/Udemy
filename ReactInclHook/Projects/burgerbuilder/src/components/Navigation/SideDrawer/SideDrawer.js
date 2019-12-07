b98977f874ab1b4f8dc4b75acb70d0db7999b541
LCLY <lchoo9294@gmail.com>
2019-12-07 22:58:56 +0800

    task: change navbar when login or logout

--- ReactInclHook/Projects/burgerbuilder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ ReactInclHook/Projects/burgerbuilder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -16,7 +16,7 @@ const SideDrawer = props => {
 					<Logo />
 				</div>
 				<nav>
-					<NavigationItems />
+					<NavigationItems isAuthenticated={props.isAuth} />
 				</nav>
 			</div>
 		</>
--- ReactInclHook/Projects/burgerbuilder/src/containers/Layout/Layout.js
+++ ReactInclHook/Projects/burgerbuilder/src/containers/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from "react";
 import classes from "./Layout.module.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+import { connect } from "react-redux";
 // turn this into a stateful component to handle backdrop
 class Layout extends React.Component {
 	state = {
@@ -21,8 +22,12 @@ class Layout extends React.Component {
 	render() {
 		return (
 			<>
-				<Toolbar drawerToggleClicked={this.sideDrawerOpenHandler} />
+				<Toolbar
+					isAuth={this.props.isAuthenticated}
+					drawerToggleClicked={this.sideDrawerOpenHandler}
+				/>
 				<SideDrawer
+					isAuth={this.props.isAuthenticated}
 					open={this.state.showSideDrawer}
 					closed={this.sideDrawerClosedHandler}
 				/>
@@ -32,4 +37,10 @@ class Layout extends React.Component {
 	}
 }
 
-export default Layout;
+const mapStateToProps = state => {
+	return {
+		isAuthenticated: state.auth.token !== null //if not null then return true
+	};
+};
+
+export default connect(mapStateToProps)(Layout);
