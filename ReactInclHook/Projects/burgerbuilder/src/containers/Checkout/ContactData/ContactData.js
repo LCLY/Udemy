2b56ee7804280990167c138dc0380ffdc2db210c
LCLY <lchoo9294@gmail.com>
2019-12-07 17:18:14 +0800

    important: added auth component

--- ReactInclHook/Projects/burgerbuilder/src/containers/Checkout/ContactData/ContactData.js
+++ ReactInclHook/Projects/burgerbuilder/src/containers/Checkout/ContactData/ContactData.js
@@ -121,6 +121,10 @@ class ContactData extends Component {
 
 	checkValidity(value, rules) {
 		let isValid = true;
+		if (!rules) {
+			return true;
+		}
+
 		if (rules.required) {
 			// remove whitespace so if theres whitespace it doenst count as a valid input
 			// value.trim() !== "" returns when you type in words because it is indeed not
@@ -131,8 +135,19 @@ class ContactData extends Component {
 		if (rules.minLength) {
 			isValid = value.length >= rules.minLength && isValid;
 		}
+
 		if (rules.maxLength) {
 			isValid = value.length <= rules.maxLength && isValid;
+		}
+
+		if (rules.isEmail) {
+			const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+			isValid = pattern.test(value) && isValid;
+		}
+
+		if (rules.isNumeric) {
+			const pattern = /^\d+$/;
+			isValid = pattern.test(value) && isValid;
 		}
 
 		return isValid;
