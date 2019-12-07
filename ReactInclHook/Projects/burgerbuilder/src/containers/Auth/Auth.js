86e017b162f09516fd3b1f286d0e684a27578adb
LCLY <lchoo9294@gmail.com>
2019-12-07 21:36:35 +0800

    task: display error message

--- ReactInclHook/Projects/burgerbuilder/src/containers/Auth/Auth.js
+++ ReactInclHook/Projects/burgerbuilder/src/containers/Auth/Auth.js
@@ -4,6 +4,7 @@ import Input from "../../components/UI/Input/Input";
 import * as actions from "../../store/actions/index";
 import classes from "./Auth.module.css";
 import { connect } from "react-redux";
+import Spinner from "../../components/UI/Spinner/Spinner";
 
 class Auth extends Component {
 	state = {
@@ -116,7 +117,7 @@ class Auth extends Component {
 			});
 		}
 
-		const form = formElementsArray.map(formElement => (
+		let form = formElementsArray.map(formElement => (
 			<Input
 				key={formElement.id}
 				elementType={formElement.config.elementType}
@@ -128,8 +129,19 @@ class Auth extends Component {
 				changed={event => this.inputChangedHandler(event, formElement.id)}
 			/>
 		));
+
+		if (this.props.loading) {
+			form = <Spinner />;
+		}
+
+		let errorMessage = null;
+		if (this.props.error) {
+			errorMessage = <p>{this.props.error.message}</p>;
+		}
+
 		return (
 			<div className={classes.Auth}>
+				{errorMessage}
 				<form onSubmit={this.submitHandler}>
 					{form}
 					<Button btnType="Success">SUBMIT</Button>
@@ -142,6 +154,13 @@ class Auth extends Component {
 	}
 }
 
+const mapStateToProps = state => {
+	return {
+		loading: state.auth.loading,
+		error: state.auth.error
+	};
+};
+
 const mapDispatchToProps = dispatch => {
 	return {
 		onAuth: (email, password, isSignUp) =>
@@ -149,4 +168,4 @@ const mapDispatchToProps = dispatch => {
 	};
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
