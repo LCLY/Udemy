01ebd245d5148930349edd092aabc040b34218c6
LCLY <lchoo9294@gmail.com>
2019-12-08 17:06:43 +0800

    important: writing jest enzyme test

---
+++ ReactInclHook/Projects/burgerbuilder/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import NavigationItems from "./NavigationItems";
+import NavigationItem from "./NavigationItem/NavigationItem";
+configure({ adapter: new Adapter() });
+
+describe("<NavigationItems />", () => {
+	let wrapper;
+	beforeEach(() => {
+		wrapper = shallow(<NavigationItems />);
+	});
+	it("should render two <NavigationItem /> elements if not authenticated", () => {
+		// inside expect we define what we want to check
+		expect(wrapper.find(NavigationItem)).toHaveLength(2);
+	});
+
+	it("should render three <NavigationItem /> elements if authenticated", () => {
+		wrapper.setProps({ isAuthenticated: true });
+		// inside expect we define what we want to check
+		expect(wrapper.find(NavigationItem)).toHaveLength(3);
+	});
+});
