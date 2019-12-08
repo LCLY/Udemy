96631b472113d7e17ef4bf38e7e8f0e7858ef91d
LCLY <lchoo9294@gmail.com>
2019-12-08 22:10:29 +0800

    built: setting up a simple react project with webpack

---
+++ ReactInclHook/practice/webpackPractice/src/containers/Users.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import PizzaImage from "../components/PizzaImage/PizzaImage";
+class Users extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Users</h1>
+				<p>User blablabla</p>
+			</div>
+		);
+	}
+}
+
+export default Users;
