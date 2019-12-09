af98cc23df18f7b88607fd8afaa9bdd4f8681e8c
LCLY <lchoo9294@gmail.com>
2019-12-09 16:37:20 +0800

    task: applied transitiongroup on modal

--- ReactInclHook/practice/animations--01-start/src/App.js
+++ ReactInclHook/practice/animations--01-start/src/App.js
@@ -53,9 +53,14 @@ class App extends Component {
 						></div>
 					)}
 				</Transition>
-				{this.state.modalIsOpen ? (
-					<Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-				) : null}
+				<Transition
+					mountOnEnter
+					unmountOnExit
+					in={this.state.modalIsOpen}
+					timeout={300}
+				>
+					{state => <Modal show={state} closed={this.closeModal} />}
+				</Transition>
 				{this.state.modalIsOpen ? (
 					<Backdrop show={this.state.modalIsOpen} />
 				) : null}
--- ReactInclHook/practice/animations--01-start/src/components/Modal/Modal.js
+++ ReactInclHook/practice/animations--01-start/src/components/Modal/Modal.js
@@ -3,7 +3,16 @@ import React from "react";
 import "./Modal.css";
 
 const modal = props => {
-	const cssClasses = ["Modal", props.show ? "ModalOpen" : "ModalClosed"];
+	const cssClasses = [
+		"Modal",
+		// if props.show is entering do modal open
+		//else if props.show is exiting then do modal closed else null
+		props.show === "entering"
+			? "ModalOpen"
+			: props.show === "exiting"
+			? "ModalClosed"
+			: null
+	];
 	return (
 		<div className={cssClasses.join(" ")}>
 			<h1>A Modal</h1>
