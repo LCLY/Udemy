4fad2bc19370153e38f609f0ff3ed22966e4ed78
LCLY <lchoo9294@gmail.com>
2019-12-10 01:13:23 +0800

    task: Posting data to firebase

--- ReactInclHook/practice/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ ReactInclHook/practice/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -7,11 +7,23 @@ import Search from "./Search";
 const Ingredients = () => {
 	const [userIngredients, setUserIngredients] = useState([]);
 	const addIngredientHandler = ingredient => {
-		// adding a new ingredient to the previous existing ingredients
-		setUserIngredients(prevIngredients => [
-			...prevIngredients,
-			{ id: Math.random().toString(), ...ingredient }
-		]);
+		fetch("https://react-hooks-812b9.firebaseio.com/ingredients.json", {
+			method: "POST",
+			body: JSON.stringify({ ingredient }),
+			headers: { "Content-Type": "application/json" }
+		})
+			.then(res => {
+				// extract body
+				return res.json();
+			})
+			.then(responseData => {
+				// adding a new ingredient to the previous existing ingredients
+				setUserIngredients(prevIngredients => [
+					...prevIngredients,
+					// .name is the unique id provided by firebase
+					{ id: responseData.name, ...ingredient }
+				]);
+			});
 	};
 
 	return (
