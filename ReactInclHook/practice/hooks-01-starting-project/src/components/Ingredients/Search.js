d07a784a86ea62f6571e76f5569b78d628c3f223
LCLY <lchoo9294@gmail.com>
2019-12-10 02:42:21 +0800

    bug: filter shows undefined object

--- ReactInclHook/practice/hooks-01-starting-project/src/components/Ingredients/Search.js
+++ ReactInclHook/practice/hooks-01-starting-project/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
@@ -6,28 +6,41 @@ import "./Search.css";
 const Search = React.memo(props => {
 	const { onLoadIngredients } = props;
 	const [enteredFilter, setEnteredFilter] = useState("");
-
+	const inputRef = useRef();
 	useEffect(() => {
-		// firebase supports filtering with query params
-		const query =
-			enteredFilter.length === 0
-				? ""
-				: `?orderBy="title"&equalTo="${enteredFilter}"`;
-		fetch("https://react-hooks-812b9.firebaseio.com/ingredients.json" + query)
-			.then(res => res.json())
-			.then(responseData => {
-				const loadedIngredients = [];
-				for (let key in responseData) {
-					loadedIngredients.push({
-						id: key,
-						title: responseData[key].ingredient.title,
-						amount: responseData[key].ingredient.amount
+		// if user stop typing for 500ms then send request
+		const timer = setTimeout(() => {
+			// if the previous character we type is the same as current
+			// then we didnt continue
+			// the inputRef is to refer to the input
+			// now inputRef is also a dependency because it could change
+			if (enteredFilter === inputRef.current.value) {
+				// firebase supports filtering with query params
+				const query =
+					enteredFilter.length === 0
+						? ""
+						: `?orderBy="title"&equalTo="${enteredFilter}"`;
+				fetch(
+					"https://react-hooks-812b9.firebaseio.com/ingredients.json" + query
+				)
+					.then(response => response.json())
+					.then(responseData => {
+						const loadedIngredients = [];
+						for (let key in responseData) {
+							if (enteredFilter.length === 0) {
+								loadedIngredients.push({
+									id: key,
+									title: responseData[key].title,
+									amount: responseData[key].amount
+								});
+							}
+						}
+						// here onLoadIngredients is a dependency
+						onLoadIngredients(loadedIngredients);
 					});
-				}
-				// here onLoadIngredients is a dependency
-				onLoadIngredients(loadedIngredients);
-			});
-	}, [enteredFilter, onLoadIngredients]);
+			}
+		}, 500);
+	}, [enteredFilter, onLoadIngredients, inputRef]);
 
 	return (
 		<section className="search">
@@ -35,6 +48,7 @@ const Search = React.memo(props => {
 				<div className="search-input">
 					<label>Filter by Title</label>
 					<input
+						ref={inputRef}
 						type="text"
 						value={enteredFilter}
 						onChange={e => setEnteredFilter(e.target.value)}
