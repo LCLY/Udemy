e09253b4ad2988fe1d8a2aad7734602b6b6cf882
LCLY <lchoo9294@gmail.com>
2019-12-09 16:45:51 +0800

    refactor: switch transition to component file itself

--- ReactInclHook/practice/animations--01-start/src/components/Modal/Modal.js
+++ ReactInclHook/practice/animations--01-start/src/components/Modal/Modal.js
@@ -1,25 +1,32 @@
 import React from "react";
 
+import { Transition } from "react-transition-group";
 import "./Modal.css";
 
 const modal = props => {
-	const cssClasses = [
-		"Modal",
-		// if props.show is entering do modal open
-		//else if props.show is exiting then do modal closed else null
-		props.show === "entering"
-			? "ModalOpen"
-			: props.show === "exiting"
-			? "ModalClosed"
-			: null
-	];
 	return (
-		<div className={cssClasses.join(" ")}>
-			<h1>A Modal</h1>
-			<button className="Button" onClick={props.closed}>
-				Dismiss
-			</button>
-		</div>
+		<Transition mountOnEnter unmountOnExit in={props.show} timeout={300}>
+			{state => {
+				const cssClasses = [
+					"Modal",
+					// if props.show is entering do modal open
+					//else if props.show is exiting then do modal closed else null
+					state === "entering"
+						? "ModalOpen"
+						: state === "exiting"
+						? "ModalClosed"
+						: null
+				];
+				return (
+					<div className={cssClasses.join(" ")}>
+						<h1>A Modal</h1>
+						<button className="Button" onClick={props.closed}>
+							Dismiss
+						</button>
+					</div>
+				);
+			}}
+		</Transition>
 	);
 };
 
