767e00bd407e332a2e3d6f54708d263bb908bbac
LCLY <lchoo9294@gmail.com>
2019-12-11 01:05:00 +0800

    refactor: ordersummary modal burgeringredient

--- ReactInclHook/Projects/burgerbuilder/src/components/Burger/OrderSummary/OrderSummary.js
+++ ReactInclHook/Projects/burgerbuilder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,39 +1,32 @@
 import React from "react";
 import Button from "../../UI/Button/Button";
-class OrderSummary extends React.Component {
-	componentWillUpdate() {
-		// console.log("[OrderSummary] update");
-	}
-	render() {
-		// console.log("the price:", this.props.price);
-		const ingredientSummary = Object.keys(this.props.ingredients).map(
-			ingKey => {
-				return (
-					<li key={ingKey}>
-						<span style={{ textTransform: "capitalize" }}>{ingKey}</span>:&nbsp;
-						{this.props.ingredients[ingKey]}
-					</li>
-				);
-			}
-		);
+const OrderSummary = props => {
+	// console.log("the price:", this.props.price);
+	const ingredientSummary = Object.keys(props.ingredients).map(ingKey => {
 		return (
-			<>
-				<h3>Your Order</h3>
-				<p>A delicious burger with the following ingredients:</p>
-				<ul>{ingredientSummary}</ul>
-				<p>Continue to Checkout?</p>
-				<p>
-					<strong>Total Price: {this.props.price.toFixed(2)}</strong>
-				</p>
-				<Button btnType="Danger" clicked={this.props.purchaseCancelled}>
-					CANCEL
-				</Button>
-				<Button btnType="Success" clicked={this.props.purchaseContinued}>
-					CONTINUE
-				</Button>
-			</>
+			<li key={ingKey}>
+				<span style={{ textTransform: "capitalize" }}>{ingKey}</span>:&nbsp;
+				{props.ingredients[ingKey]}
+			</li>
 		);
-	}
-}
+	});
+	return (
+		<>
+			<h3>Your Order</h3>
+			<p>A delicious burger with the following ingredients:</p>
+			<ul>{ingredientSummary}</ul>
+			<p>Continue to Checkout?</p>
+			<p>
+				<strong>Total Price: {props.price.toFixed(2)}</strong>
+			</p>
+			<Button btnType="Danger" clicked={props.purchaseCancelled}>
+				CANCEL
+			</Button>
+			<Button btnType="Success" clicked={props.purchaseContinued}>
+				CONTINUE
+			</Button>
+		</>
+	);
+};
 
 export default OrderSummary;
