8c9257167b43f7bbff8c8693623c8006a606c697
LCLY <lchoo9294@gmail.com>
2019-12-10 23:35:20 +0800

    refactor: withErrorHandler to func

--- ReactInclHook/Projects/burgerbuilder/src/hoc/withErrorHandler/withErrorHandler.js
+++ ReactInclHook/Projects/burgerbuilder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,50 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "../../components/UI/Modal/Modal";
 
 const withErrorHandler = (WrappedComponent, axios) => {
-	return class extends React.Component {
-		state = {
-			error: null
-		};
-		componentWillMount() {
-			this.reqInterceptor = axios.interceptors.request.use(req => {
-				this.setState({ error: null });
-				return req;
-			});
+	return props => {
+		/* ========== class========== */
+		// state = {
+		// 	error: null
+		// };
+		/* ========== functional========== */
+		const [error, setError] = useState(null);
 
-			this.resInterceptor = axios.interceptors.response.use(
-				res => res,
-				error => {
-					this.setState({ error: error });
-				}
-			);
-		}
+		const reqInterceptor = axios.interceptors.request.use(req => {
+			setError(null);
+			return req;
+		});
+
+		const resInterceptor = axios.interceptors.response.use(
+			res => res,
+			err => {
+				setError(err);
+			}
+		);
 
 		// since this anonymous class component might be used by multiple components,
 		// they will keep calling the axios interceptors even though they dont need it
 		// anymore, and that will cause memory leaks, so what we can do is to clean up
 		// the memory using componentWillUnmount
-		componentWillUnmount() {
-			axios.interceptors.request.eject(this.reqInterceptor);
-			axios.interceptors.request.eject(this.resInterceptor);
-		}
 
-		errorConfirmedHandler = () => {
-			this.setState({ error: null });
+		/* ========== class========== */
+		// componentWillUnmount() {
+		// 	axios.interceptors.request.eject(this.reqInterceptor);
+		// 	axios.interceptors.request.eject(this.resInterceptor);
+		// }
+		/* ========== functional========== */
+		// it will clean up whenever interceptors change
+		useEffect(() => {
+			return () => {
+				axios.interceptors.request.eject(reqInterceptor);
+				axios.interceptors.request.eject(resInterceptor);
+			};
+		}, [reqInterceptor, resInterceptor]);
+
+		const errorConfirmedHandler = () => {
+			setError(null);
 		};
-		render() {
-			return (
-				<>
-					<Modal
-						show={this.state.error}
-						modalClosed={this.errorConfirmedHandler}
-					>
-						{this.state.error ? this.state.error.message : null}
-					</Modal>
-					<WrappedComponent {...this.props} />
-				</>
-			);
-		}
+
+		return (
+			<>
+				<Modal show={error} modalClosed={errorConfirmedHandler}>
+					{error ? error.message : null}
+				</Modal>
+				<WrappedComponent {...this.props} />
+			</>
+		);
 	};
 };
 
