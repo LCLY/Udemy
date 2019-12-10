6d01037fc8cb48e196caa6d4b47335de0cba4878
LCLY <lchoo9294@gmail.com>
2019-12-11 01:11:06 +0800

    important: withErrorHandler custom hook

--- ReactInclHook/Projects/burgerbuilder/src/hoc/withErrorHandler/withErrorHandler.js
+++ ReactInclHook/Projects/burgerbuilder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,26 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Modal from "../../components/UI/Modal/Modal";
+
+// using custom hook
+import useHttpErrorHandler from "../../hooks/http-error-handler";
 
 const withErrorHandler = (WrappedComponent, axios) => {
 	return props => {
+		const [error, clearError] = useHttpErrorHandler(axios);
 		/* ========== class========== */
 		// state = {
 		// 	error: null
 		// };
 		/* ========== functional========== */
-		const [error, setError] = useState(null);
+		// const [error, setError] = useState(null);
 
-		const reqInterceptor = axios.interceptors.request.use(req => {
-			setError(null);
-			return req;
-		});
+		// const reqInterceptor = axios.interceptors.request.use(req => {
+		// 	setError(null);
+		// 	return req;
+		// });
 
-		const resInterceptor = axios.interceptors.response.use(
-			res => res,
-			err => {
-				setError(err);
-			}
-		);
+		// const resInterceptor = axios.interceptors.response.use(
+		// 	res => res,
+		// 	err => {
+		// 		setError(err);
+		// 	}
+		// );
 
 		// since this anonymous class component might be used by multiple components,
 		// they will keep calling the axios interceptors even though they dont need it
@@ -34,20 +38,20 @@ const withErrorHandler = (WrappedComponent, axios) => {
 		// }
 		/* ========== functional========== */
 		// it will clean up whenever interceptors change
-		useEffect(() => {
-			return () => {
-				axios.interceptors.request.eject(reqInterceptor);
-				axios.interceptors.request.eject(resInterceptor);
-			};
-		}, [reqInterceptor, resInterceptor]);
+		// useEffect(() => {
+		// 	return () => {
+		// 		axios.interceptors.request.eject(reqInterceptor);
+		// 		axios.interceptors.request.eject(resInterceptor);
+		// 	};
+		// }, [reqInterceptor, resInterceptor]);
 
-		const errorConfirmedHandler = () => {
-			setError(null);
-		};
+		// const errorConfirmedHandler = () => {
+		// 	setError(null);
+		// };
 
 		return (
 			<>
-				<Modal show={error} modalClosed={errorConfirmedHandler}>
+				<Modal show={error} modalClosed={clearError}>
 					{error ? error.message : null}
 				</Modal>
 				<WrappedComponent {...props} />
