d282b522a449f8de41250359e2f8eb9e0729fca3
LCLY <lchoo9294@gmail.com>
2019-12-12 17:13:48 +0800

    chore: add form

--- JestEnzyme/practice/notetoself/src/components/App.js
+++ JestEnzyme/practice/notetoself/src/components/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 import { Form, FormControl, Button } from "react-bootstrap";
 class App extends Component {
-	render() {
-		return (
-			<div>
-				<h1>NOTE TO SELF</h1>
-				<Form>
-					<FormControl />
-					<Button>Submit</Button>
-				</Form>
-			</div>
-		);
-	}
+  state = {
+    text: ""
+  };
+  render() {
+    return (
+      <div>
+        <h1>NOTE TO SELF</h1>
+        <Form inline>
+          <FormControl
+            onChange={e => {
+              this.setState({ text: e.target.value });
+            }}
+          />&nbsp;
+          <Button onClick={() => console.log(this.state.text)}>Submit</Button>
+        </Form>
+      </div>
+    );
+  }
 }
 export default App;
