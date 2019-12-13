7dd57affd7adc32b2f191ba9f0ee70d339e1bf0a
LCLY <lchoo9294@gmail.com>
2019-12-13 22:24:01 +0800

    refactor: splitting code into smaller components

--- JestEnzyme/practice/flashcardpro/src/components/Stack.js
+++ JestEnzyme/practice/flashcardpro/src/components/Stack.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import Card from "./Card";
 class Stack extends Component {
 	render() {
+		const { title, cards } = this.props.stack;
 		return (
 			<div>
 				<Link to="/">Home</Link>
-				<h3>Example title</h3>
+				<h3>{title}</h3>
+				<br />
+				{cards.map(card => {
+					return <Card key={card.id} card={card} />;
+				})}
 			</div>
 		);
 	}
 }
 
-export default Stack;
+const mapStateToProps = state => {
+	return { stack: state };
+};
+
+export default connect(mapStateToProps, null)(Stack);
--- JestEnzyme/practice/flashcardpro/src/index.js
+++ JestEnzyme/practice/flashcardpro/src/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Stack from "./components/Stack";
+import { setStack } from "./actions";
+import { Provider } from "react-redux";
+const store = createStore(rootReducer);
+store.subscribe(() => console.log("store", store.getState()));
+store.dispatch(setStack({ id: 0, title: "example", cards: [] }));
+
 ReactDOM.render(
-	<BrowserRouter>
-		<Switch>
-			<Route exact path="/" component={App}></Route>
-			<Route path="/stack" component={Stack}></Route>
-		</Switch>
-	</BrowserRouter>,
+	<Provider store={store}>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={App}></Route>
+				<Route path="/stack" component={Stack}></Route>
+			</Switch>
+		</BrowserRouter>
+	</Provider>,
 	document.getElementById("root")
 );
