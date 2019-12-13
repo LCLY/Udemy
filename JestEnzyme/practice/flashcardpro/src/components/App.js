594ea5eec443c3405f18603b641f75c978b40868
LCLY <lchoo9294@gmail.com>
2019-12-14 01:21:08 +0800

    test: testing rendering of components

--- JestEnzyme/practice/flashcardpro/src/components/App.js
+++ JestEnzyme/practice/flashcardpro/src/components/App.js
@@ -5,7 +5,7 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<h2>Flashcard</h2>
+				<h2>FlashCard Pro</h2>
 				<hr />
 				<StackList />
 				<hr />
--- JestEnzyme/practice/flashcardpro/src/components/App.test.js
+++ JestEnzyme/practice/flashcardpro/src/components/App.test.js
@@ -4,4 +4,17 @@ import App from "./App";
 
 describe("App", () => {
 	const app = shallow(<App />);
+	it("renders the `FlashCard Pro` title", () => {
+		// finding text of h2 element
+		expect(app.find("h2").text()).toEqual("FlashCard Pro");
+	});
+
+	it("renders the StackList", () => {
+		// console.log(app.debug());
+		expect(app.find("Connect(StackList)").exists()).toBe(true);
+	});
+
+	it("renders a link to create new stacks", () => {
+		expect(app.find("Link h4").text()).toEqual("Create a New Stack");
+	});
 });
