f4c23a7224ffe877983f7611e7deaa4dca398a38
LCLY <lchoo9294@gmail.com>
2019-12-14 22:51:37 +0800

    chore: setting up route to diff components

--- JestEnzyme/practice/jeopardy/src/index.js
+++ JestEnzyme/practice/jeopardy/src/index.js
@@ -4,12 +4,18 @@ import App from "./component/App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers";
-
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Category from "./component/Category";
 const store = createStore(rootReducer);
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={App} />
+				<Route path="/category" component={Category} />
+			</Switch>
+		</BrowserRouter>
 	</Provider>,
 	document.getElementById("root")
 );
