4fb7bfe792910785ca758aaa5950ff735fa3c23c
LCLY <lchoo9294@gmail.com>
2019-12-14 22:48:41 +0800

    chore: connecting to store

--- JestEnzyme/practice/jeopardy/src/component/App.js
+++ JestEnzyme/practice/jeopardy/src/component/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { setCategories } from "../actions";
 
 class App extends Component {
 	componentDidMount() {
-		fetch("http://jservice.io/api/categories?count=20")
-			.then(res => res.json())
-			.then(json => console.log(json));
+		if (this.props.categories.length === 0) {
+			// only fetch when nothing exists
+			fetch("http://jservice.io/api/categories?count=20")
+				.then(res => res.json())
+				.then(json => this.props.setCategories(json));
+		}
 	}
 	render() {
 		return (
@@ -15,4 +20,8 @@ class App extends Component {
 	}
 }
 
-export default App;
+const mapStateToProps = state => {
+	return { categories: state };
+};
+
+export default connect(mapStateToProps, { setCategories })(App);
